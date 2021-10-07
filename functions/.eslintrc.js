f4ab403e2841847a7ee2f7296e75d379ab5cbee3
neetibut <62812751+neetibut@users.noreply.github.com>
2021-10-07 17:22:01 +0700

    first commit

---
+++ functions/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: "@babel/eslint-parser",
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "google",
+  ],
+  rules: {
+    quotes: ["error", "double"],
+  },
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ["@babel/preset-react"],
+    },
+  },
+};
